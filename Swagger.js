bbb9de4e67979dadff47feef05a1dd2e04d7bca9
ELHart05 <o.allaoua@esi-sba.dz>
2025-02-08 06:07:17 +0100

    added cookie auth in documentation

--- Swagger.js
+++ Swagger.js
@@ -14,6 +14,20 @@ const options = {
                 url: 'http://localhost:3000',
                 description: 'Development server'
             }
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT'
+                }
+            }
+        },
+        security: [
+            {
+                bearerAuth: []
+            }
         ]
     },
     apis: ['./models/*.js', './controllers/*.js', './routes/*.js']
